3b997ac5978e2a936a37b7d3333615b8d103207f
zkboost <contact@zkboost.finance>
2023-04-06 17:46:23 +0700

    init

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x6A62c335655Fa8cef66572C611BbF8eed50BE2F0'
+
+export const INIT_CODE_HASH = '0xed9e8ad9fedb83e8e90035a2f0e20970dee0671c15535bec795f44e4b2d07f5b'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
