4bb1dbc1f40a4000ad5369c55dc929ba3a23903c
zkboost <contact@zkboost.finance>
2023-04-06 18:05:25 +0700

    v1.0.2

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,28 @@
 import JSBI from 'jsbi'
 
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  ZKSYNC_ERA_GOERLI = 280
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
 export const FACTORY_ADDRESS = '0x6A62c335655Fa8cef66572C611BbF8eed50BE2F0'
 
 export const INIT_CODE_HASH = '0xed9e8ad9fedb83e8e90035a2f0e20970dee0671c15535bec795f44e4b2d07f5b'
@@ -9,6 +32,20 @@ export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 // exports for internal consumption
 export const ZERO = JSBI.BigInt(0)
 export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
 export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
 export const _997 = JSBI.BigInt(997)
 export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
